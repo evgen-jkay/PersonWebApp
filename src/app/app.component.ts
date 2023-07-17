f96ab3f202b98f18195d808b811ef75847e06f30
evgen-jkay <evgenlandarenko@gmail.com>
2023-07-17 19:24:28 +0300

    0.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from "./models/product";
-
-import { products as data } from "./data/products";
+import { ProductsService } from "./services/products.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Eugene Web App';
 
-  products: IProduct[] = data
+  products: IProduct[] = []
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe(products => {
+      this.products = products;
+    })
+  }
 }
